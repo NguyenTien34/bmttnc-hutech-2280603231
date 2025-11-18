40456b4cb9edbf87cef1d0637852a53f7bb657c9
tiennguyenthanh2004 <tiennguyenthanh2004@gmail.com>
2025-11-18 15:26:30 +0700

    add ex02_01...05

---
+++ lab_01/ex02_01.py
@@ -0,0 +1,3 @@
+name = input("Input your name: ")
+age = input("Input your age: Tie")
+print("Welcome,", name, "You are", age, "!")
---
+++ lab_01/ex02_02.py
@@ -0,0 +1,3 @@
+r = float(input("Input radius: "))
+s = 3.14 * (r ** 2)
+print("circular area:", s)
